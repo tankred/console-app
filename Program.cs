487db9b82a10857a9f5235a7661cff5a60b425dd
Kurt Duyck <kurt.duyck@vives.be>
2020-02-19 11:17:19 +0100

    Initial commit
    	new file:   .gitignore
    	new file:   Program.cs
    	new file:   console-app.csproj
    	new file:   web.config

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+	    
+        }
+    }
+}
